8e7c9aca61d047c6a514c7c391b2891dffcd234e
zhaoxin <zhaoxin@hualala.com>
2021-04-18 11:57:42 +0800

    feat: 左侧导航功能

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
+import { Menu } from 'antd';
+// import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import MenuConfig from '../../config/menuConfig'
+import './index.less';
+
+const { SubMenu } = Menu;
 
 export default class NavLeft extends Component {
-    render () {
+    componentWillMount () {
+        const menuTreeNode = this.renderMenu(MenuConfig);
+
+        this.setState({
+            menuTreeNode
+        })
+    }
+
+    // 菜单渲染
+    renderMenu = (data) => {
+        console.log(data);
+        return data.map((item) => {
+            if (item.children) {
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        { this.renderMenu(item.children) }
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item title={item.title} key={item.key}>{ item.title }</Menu.Item>
+        })
+    }
+
+    render() {
         return (
-            <div>NavLeft content </div>
+            <div>
+                <div className="logo">
+                    <img src="/assets/logo-ant.svg" alt="" />
+                    <h1>Imooc MS</h1>
+                </div>
+
+                <Menu theme="dark">
+                    { this.state.menuTreeNode }
+                </Menu>
+            </div>
         )
     }
 }
