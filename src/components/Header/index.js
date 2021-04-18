8773d31e48a92f4f904d7253edcb59974dd7e358
zhaoxin <zhaoxin@hualala.com>
2021-04-18 14:44:51 +0800

    头部组件布局 && utils时间函数处理

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+import './index.less';
+import Util from '../../utils/utils'
 
 export default class Header extends Component {
-    render () {
+    state = {}
+
+    componentWillMount () {
+        this.setState({
+            username: "赵鑫"
+        });
+        setInterval(() => {
+            let systime = Util.formateDate(new Date().getTime());
+            this.setState({
+                systime
+            })
+        }, 1000);
+    }
+
+    render() {
         return (
-            <div>header content </div>
+            <div className="header">
+                <Row className="header-top">
+                    <Col span="24">
+                        <span>欢迎，{ this.state.username }</span>
+                        <a href="#">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span="4" className="breadcrumb-title">
+                        首页
+                    </Col>
+                    <Col span="20" className="weather">
+                        <span className="date">{ this.state.systime }</span>
+                        <span className="weather-detail">晴朗</span>
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
