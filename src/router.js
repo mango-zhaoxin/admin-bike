2ce76a2a9732b8335535ce232de5325966e41a01
zhaoxin <zhaoxin@hualala.com>
2021-04-22 22:11:18 +0800

    feat: 通知提醒框notification的使用

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import Admin from './admin';
 import Buttons from './pages/ui/buttons';
 import Loadings from './pages/ui/loadings';
 import Modals from './pages/ui/modals';
+import Notice from './pages/ui/notice';
 import NoMatch from './pages/noMatch';
 
 export default class IRouter extends React.Component {
@@ -21,6 +22,7 @@ export default class IRouter extends React.Component {
                                     <Route path="/admin/ui/buttons" component={Buttons} />
                                     <Route path="/admin/ui/modals" component={Modals} />
                                     <Route path="/admin/ui/loadings" component={Loadings} />
+                                    <Route path="/admin/ui/notification" component={Notice} />
                                     <Route component={NoMatch} />
                                 </Switch>
                             </Admin>
